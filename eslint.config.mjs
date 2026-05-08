58bef2afc32074a7e19e6b18b1a3253e5579d26c
chendong <240259939@qq.com>
2026-05-08 17:07:25 +0800

    chore: support vue linting in workspace

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,6 @@
 import js from '@eslint/js';
+import vue from 'eslint-plugin-vue';
+import vueParser from 'vue-eslint-parser';
 import tseslint from 'typescript-eslint';
 
 export default [
@@ -12,9 +14,18 @@ export default [
     ]
   },
   js.configs.recommended,
+  ...vue.configs['flat/recommended'],
   ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx,vue}'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        extraFileExtensions: ['.vue'],
+        sourceType: 'module'
+      }
+    },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off'
     }
