4163e217fdca4b315c8ea6a2401f4cbd9118ea66
chendong <240259939@qq.com>
2026-05-10 14:51:47 +0800

    chore: update husky commands to use pnpm and improve button type description

--- packages/vue/src/components/button/props.ts
+++ packages/vue/src/components/button/props.ts
@@ -12,7 +12,7 @@ export type ButtonGroupDirection = 'horizontal' | 'vertical';
 
 export const ButtonProps = {
   /**
-   * 按钮类型测试1
+   * 按钮类型
    *
    * 用于控制按钮的语义色，可选值为 `default`、`primary`、`success`、`info`、`warning`、`error`。
    */
